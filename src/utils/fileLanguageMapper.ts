c4abd4e3503e7ede5fadd6fb18093c765ab7b4de
dimabreus <dmitriybreus5@gmail.com>
2024-05-26 17:54:12 +0300

    added some langs

--- src/utils/fileLanguageMapper.ts
+++ src/utils/fileLanguageMapper.ts
@@ -1,9 +1,14 @@
 function fileLanguageMapper(extension: string): string {
     const mapping: { [key: string]: string } = {
         'js': 'javascript',
+        'cjs': 'javascript',
         'ts': 'typescript',
+        'tsx': 'typescript',
+        'json': 'json',
+        'svg': 'svg',
         'py': 'python',
         'md': 'markdown',
+        'html': 'html',
         'txt': 'plaintext'
     };
 
