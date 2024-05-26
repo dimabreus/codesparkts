e80d291c950e727cf8fb04895b94e2e820898178
dimabreus <dmitriybreus5@gmail.com>
2024-05-26 18:37:50 +0300

    added read directory

--- app/ipcMain.cjs
+++ app/ipcMain.cjs
@@ -1,32 +1,6 @@
 const { ipcMain, dialog } = require('electron');
-const fs = require('fs').promises;
-const { exec } = require('child_process');
-let currentDir = process.cwd();  // начальная директория установлена на директорию запуска приложения
-
-ipcMain.handle('run-command', (event, command) => {
-    // Обработка команды cd особенно, чтобы обновить currentDir
-    if (command.trim().startsWith('cd ')) {
-        const newDir = command.trim().substring(3).trim();
-        try {
-            process.chdir(newDir);  // пытаемся сменить директорию
-            currentDir = process.cwd();  // обновляем текущую директорию
-            return Promise.resolve(`Changed directory to ${currentDir}`);
-        } catch (error) {
-            return Promise.reject(`Error changing directory: ${error.message}`);
-        }
-    }
-
-    const powershellCommand = `$OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; ${command}`;
-    return new Promise((resolve, reject) => {
-        exec(powershellCommand, { shell: 'bash', cwd: currentDir, encoding: 'utf8' }, (error, stdout, stderr) => {
-            if (error) {
-                reject(stderr);
-            } else {
-                resolve(stdout);
-            }
-        });
-    });
-});
+const fs = require('fs');
+const path = require('path');
 
 ipcMain.handle('dialog:openFile', async () => {
     const { filePaths } = await dialog.showOpenDialog({
@@ -47,9 +21,41 @@ ipcMain.handle('dialog:saveFile', async () => {
 });
 
 ipcMain.handle('read-file', async (event, filePath) => {
-    return fs.readFile(filePath, 'utf-8');
+    return fs.promises.readFile(filePath, 'utf-8');
 });
 
 ipcMain.handle('save-file', async (event, filePath, content) => {
-    return fs.writeFile(filePath, content, 'utf-8');
+    return fs.promises.writeFile(filePath, content, 'utf-8');
+});
+
+function readFiles(directoryPath) {
+    const result = [];
+    const files = fs.readdirSync(directoryPath);
+
+    files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
+
+        const stats = fs.statSync(filePath);
+
+        const item = {
+            dir: filePath,
+        };
+
+        if (stats.isFile()) {
+            item.type = 'file';
+            item.filename = file;
+        } else if (stats.isDirectory()) {
+            item.type = 'dir';
+            item.dirname = file;
+            item.files = readFiles(filePath);
+        }
+
+        result.push(item);
+    });
+
+    return result;
+};
+
+ipcMain.handle('read-directory', async (event, directory) => {
+    return readFiles(directory)
 });
